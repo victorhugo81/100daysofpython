129556f972663e2b707d580209b0fffe371ed108
Parallels <victorhugo81@yahoo.com>
2026-02-06 14:37:34 -0800

    update day001 project

--- day001.py
+++ day001.py
@@ -1,13 +1,19 @@
 # ======================
 # Day 1: Band Name Generator
+# Instructions: Working with variables in Python to manage user input data. 
 # ======================
+
 
 def main():
     print("Welcome to the Band Name Generator")
+
+    # Get user input for city and pet name
     city = input("Which city did you grow up in?\n")
     pet = input("What is the name of a pet?\n")
+
+    # Combine the city and pet name to create a band name
     print("Your band name could be: " + city + " " + pet)
 
-
+# Run the main function when the script is executed
 if __name__ == "__main__":
     main()
