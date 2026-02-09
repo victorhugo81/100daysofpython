a77bdabecb1c2655939ba182b4ad984c4009e541
Victor Hugo <victorhugo81@yahoo.com>
2026-02-09 12:14:09 -0800

    upload day004

--- day004/day004.py
+++ day004/day004.py
@@ -1,9 +1,9 @@
 # ======================
 # Project Name: Day 4 - The Rock, Paper, Scissors Game
 # Section: Beginner Python Projects
-# Description: Randomisation and Python Lists
+# Description: Randomization and Python Lists
 # ======================
-
+import random
 
 def main():
     # ASCII art for the game
@@ -29,24 +29,46 @@ def main():
     """
 
     scissors = """
-            __ 
-          /   )
-    ---''    /_____
-            __ ____)
-            ___ ____)
-          <..
-         ..   '*,          
-           '* ._ )
-    ---_ _ '- .__ )
-
+        ______
+    ---'  ____)___
+            ______)
+        __________)
+        (____)
+    ---.__(___)
     """
 
-    print("Welcome to the Rock, Paper, Scissors Game!" + "\n" + rock)
+    game_images = [rock, paper, scissors]
+
+    print("Welcome to the Rock, Paper, Scissors Game!\n")
+    print("Type a number to play the game. If you type an invalid number or character, you lose!\n")
+    print("Rock = 0\nPaper = 1\nScissors = 2\n")
 
 
+    # User input
+    user_choice = input("What do you choose? ")
+    # User choice with input validation
+    if user_choice.isdigit() and int(user_choice) in [0, 1, 2]:
+        user_choice = int(user_choice)
+        print(game_images[int(user_choice)])
 
+        # Computer choice
+        computer_choice = random.randint(0, 2)
+        print(f"Computer chose: {game_images[computer_choice]}")
 
+        # comparing player choice
+        if user_choice == 0 and computer_choice == 2:
+            print("You win!")
+        elif user_choice == 1 and computer_choice == 0:
+            print("You win!")
+        elif user_choice == 2 and computer_choice == 1:
+            print("You win!")
+        elif user_choice == computer_choice:
+            print("It's a draw!")
+        else:
+            print("You lose!")
 
+    else:
+        print("You typed an invalid number or character. You lose!")
 
 
 
