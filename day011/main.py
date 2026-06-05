9502c3fef812bc339e614a7ec1d6fe44e8bbf7b8
Victor Solis <victorhugo81@yahoo.com>
2026-06-05 11:44:59 -0700

    starting project

--- day011/main.py
+++ day011/main.py
@@ -6,11 +6,37 @@
 # ======================
 
 # Import ASCII logo from external file
-from art import LOGO   
+from art import LOGO  
+from random import random
+ 
 
 # Display the blackjack logo and welcome message
 print(LOGO)
 print("*******************************************")
 print("Welcome to The Blackjack Game!")
 print("*******************************************")
+
+
+def deal_card():
+    # Returns a random card from the deck.
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] 
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+def calculate_score(cards):
+    # Calculate the score of the given cards.
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0  # Blackjack
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 
