98bc4ccf98a61d7e248454a3f7517eea00890c08
Victor Solis <victorhugo81@yahoo.com>
2026-06-05 09:21:03 -0700

    initial commit

---
+++ day011/main.py
@@ -0,0 +1,16 @@
+# ======================
+# Project Name: Day 11 - Blackjack Game
+# Section: Beginner Python Projects
+# Description: A simple command-line Blackjack game where the user plays against the computer (dealer). The game includes features such as card dealing, 
+#               score calculation, and determining the winner based on standard Blackjack rules.
+# ======================
+
+# Import ASCII logo from external file
+from art import LOGO   
+
+# Display the blackjack logo and welcome message
+print(LOGO)
+print("*******************************************")
+print("Welcome to The Blackjack Game!")
+print("*******************************************")
+
